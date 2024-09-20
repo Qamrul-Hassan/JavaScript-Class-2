15333bcdd22b236fc229c638ca39f70bb0c78788
Qamrul-Hassan <your-email@example.com>
2024-09-20 16:40:09 +0600

    massage

---
+++ q12.js
@@ -0,0 +1,5 @@
+let long ="apple banana cherry date elderberry fig grape honeydew kiwi lemon mango nectarine orange papaya rasberry tangerine ugli\
+vine watermelon yam zucchini apricot blackberry cantaloupe eldeberry fennel grapefruit kumquat lime melon nectar olive peach pear\
+plum pomegranate rhubarb starfruit tomato vanilla";
+let index = long.indexOf("fennel");
+console.log(index);
