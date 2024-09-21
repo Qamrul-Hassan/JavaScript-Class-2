06adb80eae874cfa8b1a022d317706f4067b34f5
Qamrul-Hassan <your-email@example.com>
2024-09-21 15:04:36 +0600

    massage

--- q18.js
+++ q18.js
@@ -1,14 +1,14 @@
-//using push & splice
+//using push & slice
 
 let numbers = [1, 2, 3];
 let merge = numbers.push (4,5,6);
 console.log (numbers);
-let get = numbers.splice(0,4);
+let get = numbers.slice(0,4);
 console.log(get);
 
 //using cocat & slice
 
-let no1 = [1,2,3];
+let no1 = [1, 2, 3];
 let no2 = [4, 5, 6];
 let mrg = no1.concat(no2);
 console.log(mrg);
---
+++ q20.js
@@ -0,0 +1,8 @@
+let no = [10, 20, 30, 40, 50];
+let subary = no.slice(1,4);
+console.log(subary);
+
+// slice(1, 4):
+// 1: Start from index 1 (which is 20).
+// 4: Stop at index 4 (excluding 50 at index 4).
+// The result is [20, 30, 40]. 
