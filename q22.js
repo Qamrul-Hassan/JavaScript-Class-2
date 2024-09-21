196c6bc4173db1a9ece3c1b6840a6033cce97f8d
Qamrul-Hassan <your-email@example.com>
2024-09-21 16:32:30 +0600

    massage

---
+++ q22.js
@@ -0,0 +1,8 @@
+let aray =["a", "b", "c", "d", "e", "f"];
+let firstAray = aray.at();
+let lastAray = aray.at(-1);
+
+let result =(firstAray);
+let result2 = (lastAray);
+
+console.log(result,result2);
