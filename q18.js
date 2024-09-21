ffc49d4be0864ff65a367b3a5bbf70be61cb6634
Qamrul-Hassan <your-email@example.com>
2024-09-21 14:49:00 +0600

    massage

--- q18.js
+++ q18.js
@@ -1,5 +1,17 @@
+//using push & splice
+
 let numbers = [1, 2, 3];
 let merge = numbers.push (4,5,6);
 console.log (numbers);
 let get = numbers.splice(0,4);
 console.log(get);
+
+//using cocat & slice
+
+let no1 = [1,2,3];
+let no2 = [4, 5, 6];
+let mrg = no1.concat(no2);
+console.log(mrg);
+
+let gets = mrg.slice(0,4);
+console.log(gets);
---
+++ q19.js
@@ -0,0 +1,3 @@
+let letters = ["a", "b", "c", "d", "e"];
+letters.splice (1,2, "x", "y");
+console.log(letters);
